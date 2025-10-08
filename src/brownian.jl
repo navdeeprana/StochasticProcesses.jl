481ef5c67320703f19ba5fd38ac4611e65d0be3d
navdeep rana <navdeepr@tifrh.res.in>
2025-10-08 11:26:41 +0200

    move weiner process code to brownian

--- src/brownian.jl
+++ src/brownian.jl
@@ -1,22 +1,64 @@
+function weiner_increment(N, s)
+    dW = randn(N)
+    @. dW *= s
+    return dW
+end
+
+weiner_process(dW::AbstractVector) = pushfirst!(cumsum(dW), 0.0)
+
 # Sample a Brownian motion or a Wiener process from [0, tmax] with a time step Δt.
 # Returns time and the Wiener process.
-function brownian_motion(Δt, tmax)
-    N = round(Int, tmax / Δt)
-    t = Δt .* (0:1:N)
-    W = zeros(N + 1)
-    for i in 1:N
-        W[i+1] = W[i] + sqrt(Δt) * randn()
-    end
+function weiner_process(h, tmax)
+    N = round(Int, tmax / h)
+    t = h .* (0:1:N)
+    W = weiner_process(weiner_increment(N, sqrt(h)))
     return t, W
 end
 
 # Sample an ensemble of size nens of Brownian motions from [0, tmax] with a time step Δt.
 # Returns time and a dataframe with all the Wiener processes.
-function brownian_motion(Δt, tmax, nens)
-    t, W1 = brownian_motion(Δt, tmax)
-    df = DataFrame(W1 = W1)
-    for e in 2:nens
-        df[!, "W$e"] = brownian_motion(Δt, tmax)[2] # Drop the time
-    end
+function weiner_process(h, tmax, nens)
+    N, sqrth = round(Int, tmax / h), sqrt(h)
+    t = h .* (0:1:N)
+    df = DataFrame([Symbol("W$e") => weiner_process(weiner_increment(N, sqrth)) for e in 1:nens])
     return t, df
 end
+
+abstract type AbstractWeinerIncrement end
+
+struct InstantWeinerIncrement{T} <: AbstractWeinerIncrement
+    h::T
+    sqrth::T
+end
+
+InstantWeinerIncrement(h::T) where {T} = InstantWeinerIncrement{T}(h, sqrt(h))
+
+struct ComputedWeinerIncrement{T} <: AbstractWeinerIncrement
+    h::T
+    sqrth::T
+    W::Vector{T}
+end
+
+function ComputedWeinerIncrement(h::T, tmax::T) where {T}
+    t, W = brownian_motion(h, tmax)
+    ComputedWeinerIncrement{T}(h, sqrt(h), W)
+end
+
+struct SampledWeinerIncrement{T} <: AbstractWeinerIncrement
+    h::T
+    sqrth::T
+    dW::Vector{T}
+end
+
+function SampledWeinerIncrement(h::T, tmax::T) where {T}
+    N = round(Int, tmax/h)
+    SampledWeinerIncrement{T}(h, sqrt(h), weiner_increment(N, sqrt(h)))
+end
+
+Base.getindex(dW::SampledWeinerIncrement, i) = dW.dW[i]
+Base.getindex(dW::InstantWeinerIncrement, i) = dW.sqrth * randn()
+Base.getindex(dW::ComputedWeinerIncrement, i) = dW.W[i+1] - dW.W[i]
+
+weiner_process(dW::SampledWeinerIncrement) = weiner_process(dW.dW)
+
+brownian_motion(args...; kwargs...) = weiner_process(args...; kwargs...)
--- src/solve.jl
+++ src/solve.jl
@@ -17,25 +17,28 @@ Milstein(h) = Integrator(Milstein(), (h = h, sqrth = sqrt(h)))
 # For left-point approximation, approx = 1.0 and for mid-point approximation, approx = 0.5.
 # By default we choose mid-point approximation as it is more accurate.
 
-function SETDEulerMaruyama(h, c, approx=0.5)
+function SETDEulerMaruyama(h, c, approx = 0.5)
     fac = (exp(c*h), expm1(c*h)/c, exp(c*h*approx))
     Integrator(SETDEulerMaruyama(), (; h, sqrth = sqrt(h), fac))
 end
 
-function SETDMilstein(h, c, approx=0.5)
+function SETDMilstein(h, c, approx = 0.5)
     fac = (exp(c*h), expm1(c*h)/c, exp(c*h*approx))
     Integrator(SETDMilstein(), (; h, sqrth = sqrt(h), fac))
 end
 
 function SETD1(h, c)
+    # Since these integrators operate on Weiner increments, and not directly on the random
+    # numbers, we need to scale the stochastic integral by sqrt(h). Technically SETD1 should
+    # only operate on InstantWeinerIncrement, but we leave it this way.
     fac = (exp(c*h), expm1(c*h)/c, sqrt(expm1(2*c*h)/(2c))/sqrt(h))
     Integrator(SETD1(), (; h, sqrth = sqrt(h), fac))
 end
 
-struct SODE{U,P}
-    f::Function
-    g::Function
-    dg::Function
+struct SODE{F,G,DG,P}
+    f::F
+    g::G
+    dg::DG
     p::P
 end
 
@@ -68,91 +71,21 @@ function stepforward(::SETD1, q, s::SODE, u0, dW)
     return q.fac[1] * u0 + q.fac[2] * s.f(u0, s.p) + q.fac[3] * s.g(u0, s.p) * dW
 end
 
-abstract type AbstractWeinerIncrement end
-
-struct SampledWeinerIncrement{T} <: AbstractWeinerIncrement
-    h::T
-    sqrth::T
-    dW::Vector{T}
-end
-
-function SampledWeinerIncrement(h::T) where {T}
-    SampledWeinerIncrement{T}(h, sqrt(h), Float64[])
-end
-
-function SampledWeinerIncrement(h::T, tmax::T) where {T}
-    dW = zeros(round(Int, tmax/h))
-    randn!(dW)
-    @. dW *= sqrt(h)
-    SampledWeinerIncrement{T}(h, sqrt(h), dW)
-end
-
-function brownian_motion(dW::SampledWeinerIncrement)
-    W = [0.0]
-    Wi = 0.0
-    for dWi in dW.dW
-        Wi += dWi
-        push!(W, Wi)
-    end
-    return W
-end
-
-struct InstantWeinerIncrement{T} <: AbstractWeinerIncrement
-    h::T
-    sqrth::T
-end
-
-function InstantWeinerIncrement(h::T) where {T}
-    InstantWeinerIncrement{T}(h, sqrt(h))
-end
-
-struct ComputedWeinerIncrement{T} <: AbstractWeinerIncrement
-    h::T
-    sqrth::T
-    W::Vector{T}
-end
-
-function ComputedWeinerIncrement(h::T, tmax::T) where {T}
-    t, W = brownian_motion(h, tmax)
-    ComputedWeinerIncrement{T}(h, sqrt(h), W)
-end
-
-Base.getindex(dW::SampledWeinerIncrement, i) = dW.dW[i]
-Base.getindex(dW::InstantWeinerIncrement, i) = dW.sqrth * randn()
-Base.getindex(dW::ComputedWeinerIncrement, i) = dW.W[i+1] - dW.W[i]
-
-@inline _push!(::Val{true}, x, e) = push!(x, e)
-@inline _push!(::Val{false}, x, e) = nothing
-
-function solve(
-    sde::SODE,
-    int::Integrator,
-    dW::AbstractWeinerIncrement,
-    u0,
-    tmax,
-    saveat;
-    save_after = 0.0,
-    save_noise = false,
-    save_initial = true
-)
+function solve(s::SODE, int::Integrator, dW::AbstractWeinerIncrement, u0, tmax, saveat; save_after = 0.0)
     (; h, sqrth) = int.q
     niters, nsave = @. round(Int, (tmax, saveat)/h)
-    save_noise_val = Val(save_noise)
-    sol = (; t = [0.0], u = [u0], W = [0.0])
-    if !save_initial
-        pop!(sol.t);
-        pop!(sol.u);
-        pop!(sol.W)
+    sol = (; t = Float64[], u = Float64[])
+    if save_after == 0.0
+        push!(sol.t, 0.0);
+        push!(sol.u, u0);
     end
-    ui, Wi = u0, 0.0
+    ui = u0
     for i in 1:niters
         dWi = dW[i]
-        Wi += dWi
-        ui = stepforward(int, sde, ui, dWi)
+        ui = stepforward(int, s, ui, dWi)
         if (mod(i, nsave) == 0) && (i*h >= save_after)
             push!(sol.t, i*h)
             push!(sol.u, ui)
-            _push!(save_noise_val, sol.W, Wi)
         end
     end
     return sol
@@ -166,8 +99,7 @@ function compute_convergence!(cvg, h, sol, sol_an, mean_an)
         es = mean(@. abs(u - uan))
         ew = abs(mean(u) - mean(uan))
         ew_an = abs(mean(u) - mean_an[i])
-        ci = 1.96 * std(u) / sqrt(length(sol))
-        push!(cvg, (ti, h, es, ew, ew_an, ci))
+        push!(cvg, (ti, h, es, ew, ew_an))
     end
 end
 
@@ -177,17 +109,15 @@ function tnWn(t, W, twhen)
     return tn, Wn
 end
 
-function convergence(sde, integrator, pars, h_cvg, t, W, sol_an, mean_an)
-    cvg = DataFrame(
-        t = Float64[], h = Float64[], es = Float64[], ew = Float64[], ew_an = Float64[], ci = Float64[]
-    )
+function convergence(s, int_constructor, p, h_cvg, t, W, sol_an, mean_an)
+    cvg = DataFrame(t = Float64[], h = Float64[], es = Float64[], ew = Float64[], ew_an = Float64[])
     for h in h_cvg
         tn, Wn = tnWn(t, W, h)
         sol = map(
             Wni -> solve(
-                sde, integrator(h),
+                s, int_constructor(h),
                 ComputedWeinerIncrement{typeof(h)}(h, sqrt(h), Wni),
-                pars.u0, pars.tmax, pars.saveat; save_noise = true
+                p.u0, p.tmax, p.saveat
             ),
             eachcol(Wn)
         )
